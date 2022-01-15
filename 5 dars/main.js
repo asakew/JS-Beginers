a7d977c240ffb6a73156ab9fdcc727fe00defd72
asakew <asauz777@gmail.com>
2022-01-15 14:35:31 +0500

    // 5 dars: Kontakenatsiya qator va shablon qator

---
+++ 5 dars/main.js
@@ -0,0 +1,9 @@
+// 5 dars: Kontakenatsiya qator va shablon qator
+
+// Kontakenatsiya misol
+const userName = 'asakew';
+console.log('Salom ' + userName + ' xush kelibsiz');
+
+// Shablon qator misol
+// Shablon qator va imperpalatsiyaga misol
+console.log(`Salom  ${userName}  yana sizmi?`);
