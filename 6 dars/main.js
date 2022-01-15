fec74ce67041f18afd112a46a97149c6a94d514c
asakew <asauz777@gmail.com>
2022-01-15 15:29:33 +0500

    6.1 Funtions: Parametrlari va agrumaentlari

--- 6 dars/main.js
+++ 6 dars/main.js
@@ -2,22 +2,22 @@
 // DRY - Don't repeat youself
 
 // 1 examples
-function userHi() {
+/*function userHi2() {
     console.log('Hi user');
 }
 
-//.userHi();
+userHi2();
 
 // Function Declaration
 //userHi();
 function userHi() {
-    alert('hello')
+    console.log('test');
 }
 // userHi(); js tepadan pastga qarab kodlarni uqiydi va userHi(); yuqoriga yoki pastga bulsaham ishlayveradi.
-
+*/
 // Function Experssion
 // let, const, var - axamiyati yuq
 const userHi = function() {
-    alert('hello')
+    console.log('hi asa');
 }
 userHi(); // esa faqat function-dan keyin ishlaydi
---
+++ 7 dars/main.js
@@ -0,0 +1,9 @@
+// 6.1 Funtions: Parametrlari va agrumaentlari
+
+function user(name) {
+    console.log(`Salom ${name}!`);
+}
+
+user('Akmal');
+user('Akbar');
+user('Anorbek');
