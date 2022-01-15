cc06cb9a18567643029f309cda78b09d1af59079
asakew <asauz777@gmail.com>
2022-01-15 16:53:43 +0500

    Short Funtions codes

---
+++ 6.5 short functions/main.js
@@ -0,0 +1,10 @@
+// v1
+const funName = (userName) => {
+    console.log(`hi ${userName}!`);
+}
+funName('asakew');
+
+// v2 agar function-ga bir qatirli kod bulsa
+const fun2Name = (name) => console.log(`hi ${name}!`);
+
+fun2Name('Akmal')
