0bb93bc7ff4f098b76a441ce5806d70fa9e2bc66
asakew <asauz777@gmail.com>
2022-01-15 12:03:03 +0500

    up

---
+++ main.js
@@ -0,0 +1,40 @@
+/* Beginerlar uchun:
+Numbers
+qator strong
+bolean
+null
+undefiend
+object
+massiv
+
+bigin
+sumbol - lar keynchalikham urgansa buladi
+*/
+
+let userName = 'asakew';
+
+let age = 23;
+//let age = null;
+let year = 1999;
+
+
+let uydaSved = true;
+
+
+// Dinamitiskiy tipazatsiya
+let someVariable = 'some value';
+someVariable = 50;
+someVariable = true; // Boolean
+
+// undefined
+let city;
+
+// operator: typeof - kodlarni qaney turga tegishliligini tekshirish uchun ishlatiladi, misol uchun number, text, null...undefined
+const lastName = 'Akmal';
+// const age = '23';
+// console.log(typeof age) - ikkinchi typeof ishlamas ekan
+
+// conlosega max saqlash Number.MAX_SAFE_INTEGER);
+// va 90071992547409913637373838n - qilibham chiqorsak bularkan
+
+console.log(userName,age,year,someVariable,city, typeof lastName, Number.MAX_SAFE_INTEGER);
