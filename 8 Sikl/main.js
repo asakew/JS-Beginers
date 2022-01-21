549ac5f884e49a94f33d7d2fdfbc8b22c460b525
asakew <asauz777@gmail.com>
2022-01-21 12:43:41 +0500

    sikl

---
+++ 8 Sikl/main.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+// for (var i = 0; i < 10; i = i + 2) {...}
+
+// 8-martga qizlarga sovg'a xD
+//for (var i = 0; true; i++) {
+ // console.log(i);
+//}
+
