5908a440c2a5d6c873b2f01006d71246ad28bf55
asakew <asauz777@gmail.com>
2022-01-21 16:08:29 +0500

    DOM element tanlash

---
+++ 13. DOM element tanlash/main.js
@@ -0,0 +1,11 @@
+// document.querySelector('selector, HTML, CSS classlarni nomini yozish mumkin, va faqat bitta tepadan keladigan elemnni tanlaydi ikkinchisini emas!');
+
+// 1-misol
+document.querySelector('h1').classList.add('cyan')
+
+// 2-misol
+const parag = document.querySelector('p');
+parag.classList.add('cyan');
+
+
+
---
+++ note.md
@@ -0,0 +1,4 @@
+## short key: VS Code Studio
+Barcha ochilgan teg, kodlarni qisqartirib yopib quyadi DOM elementlarga qulay iwlaw:
+
+'Ctrl + L: Ctrl + 0' - nolni numPaddan bosmaslik kk
